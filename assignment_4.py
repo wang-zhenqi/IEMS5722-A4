7b095d71b6350c84c28d07205c8e84cdf8a9b8a3
zhenqi <dreamingwzq@gmail.com>
2020-04-17 21:23:14 +0800

    Assignment-4: add broadcast api, waiting for webserver to call

--- assignment_4.py
+++ assignment_4.py
@@ -3,14 +3,23 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iems572230347'
+app.debug=True
 
 socketio = SocketIO(app)
 
 @app.route("/api/a4/broadcast_room", methods=["GET"])
 def broadcast_room():
+    result = {}
     chatroom_id = int(request.args.get('chatroom_id'))
     message = request.args.get('message')
-    #socketio.emit(...)
+    if not (chatroom_id and message):
+        result['message'] = 'Invailid Parameter'
+        result['status'] = 'Error'
+        return jsonify(result)
+    result['chatroom_id'] = chatroom_id
+    result['message'] = message
+    socketio.emit('new_message', {'text': 'message'}, room=chatroom_id)
+    return jsonify(result)
 
 @socketio.on('my event')
 def my_event_handler(data):
@@ -18,10 +27,14 @@ def my_event_handler(data):
 
 @socketio.on('join')
 def on_join(data):
+    chatroom_id = data['chatroom_id']
+    print(chatroom_id)
     join_room(chatroom_id)
 
 @socketio.on('leave')
 def on_leave(data):
+    chatroom_id = data['chatroom_id']
+    print(chatroom_id)
     leave_room(chatroom_id)
 
 if __name__ == '__main__':
